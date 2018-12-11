c024aa42c8511c6771bd3113fee620481ce46db0
user <user@192.168.4.117>
2018-12-11 14:59:50 +0900

    logindao, service 수정

--- StationTrip/src/main/java/com/hk/trip/model/LoginDao.java
+++ StationTrip/src/main/java/com/hk/trip/model/LoginDao.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class LoginDao {
+public class LoginDao implements ILoginDao {
 
 	@Autowired
 	private SqlSessionTemplate sqlSession;
